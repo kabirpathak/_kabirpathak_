00b0b5e195f22d73f4504329304dde16f7572098
kabirpathak <kabirpathak99@gmail.com>
2022-09-30 09:55:38 +0530

    [UPDATE] Separated tests.

---
+++ test/integration/admin/admin_flow_test.rb
@@ -0,0 +1,12 @@
+class AdminFlowTest < ActionDispatch::IntegrationTest
+    def setup
+    end
+  
+    def teardown
+    end
+  
+    def test_admin
+        puts 'admin test'
+    end
+  end
+  
---
+++ test/integration/client/client_flow_test.rb
@@ -0,0 +1,11 @@
+class ClientFlowTest < ActionDispatch::IntegrationTest
+  def setup
+  end
+
+  def teardown
+  end
+
+  def test_client
+    puts 'client test'
+  end
+end
